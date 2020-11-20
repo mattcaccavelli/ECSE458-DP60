0980e9d50ecf1b7e551d5b7d2e5df82baa23e5e7
Dian <dianbasit@gmail.com>
2020-11-19 18:26:08 -0700

    Revert "Updated PID values and test"
    This reverts commit 3e5ff02e62a624a37431adfab7d17573f5874c79.

--- Arduino/Quadcopter Phase 2/SR04_test/SR04_test.ino
+++ Arduino/Quadcopter Phase 2/SR04_test/SR04_test.ino
@@ -1,42 +1,20 @@
 #include <HCSR04.h>
 
-int triggerPin = 4;
-int echoPin = 5;
+int triggerPin = 2;
+int echoPin = 3;
 
-double duration, distance;
 void setup () {
   Serial.begin(9600);
   HCSR04.begin(triggerPin, echoPin);
-  // Extra
-  pinMode(triggerPin, OUTPUT);
-  pinMode(echoPin, INPUT);
 }
 
 void loop () {
-//  double* distances = HCSR04.measureDistanceCm();
-  distance = 0;
-  for (int i = 0; i< 10; i++){
-    digitalWrite(triggerPin, LOW);
-    delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(triggerPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(triggerPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
-    distance += duration*0.034/2;  
-  }
-  distance/=10;
-// Clears the trigPin
+  double* distances = HCSR04.measureDistanceCm();
   
-// Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-//  Serial.print("1: ");
-//  Serial.print(distances[0]);
-//  Serial.println("cm");
-//  
+  Serial.print("1: ");
+  Serial.print(distances[0]);
+  Serial.println("cm");
+  
   Serial.println("---");
   delay(500);
 }
